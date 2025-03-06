a429ba8a76e5f512d88d3b4ac09c27ff09710734
core2drew <calupesandro@gmail.com>
2025-03-06 19:47:59 +0800

    chore: update app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,16 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { firebaseConfig } from '../../firebase/firebase.config';
+import { environment } from '../../environments/environment';
 
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig },
   ],
 };

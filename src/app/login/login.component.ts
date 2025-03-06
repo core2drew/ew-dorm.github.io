aebb4dc5b2d514cc09bfe7a13b420ec7e5aa09e2
core2drew <calupesandro@gmail.com>
2025-03-06 23:56:23 +0800

    refactor: login component to use primeng

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-
+import { AuthService } from '../services/auth/auth.service';
+import { CardModule } from 'primeng/card';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { AUTH_ERROR_CODE } from '../enums/auth-error-code';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { Ripple } from 'primeng/ripple';
 @Component({
   selector: 'ds-login',
   imports: [
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
+    CardModule,
     ReactiveFormsModule,
-    MatButtonModule,
-    MatIconModule,
+    IftaLabelModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+    Ripple,
   ],
+  providers: [AuthService, MessageService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
+  standalone: true,
 })
 export class LoginComponent {
   emailFormControl = new FormControl('', [
@@ -25,4 +31,24 @@ export class LoginComponent {
     Validators.email,
   ]);
   passwordFormControl = new FormControl('', [Validators.required]);
+
+  constructor(
+    private authService: AuthService,
+    private messageService: MessageService,
+  ) {}
+
+  signIn() {
+    this.authService
+      .signIn(this.emailFormControl.value!, this.passwordFormControl.value!)
+      .then((user) => console.log('Login successful:', user))
+      .catch((errorCode) => {
+        console.log(errorCode);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Something went wrong',
+          life: 3000,
+        });
+      });
+  }
 }

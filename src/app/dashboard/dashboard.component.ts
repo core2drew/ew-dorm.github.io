4a3414fde65dd0f7f62699f8508c0652b46afec5
core2drew <calupesandro@gmail.com>
2025-03-06 23:56:37 +0800

    chore: add sign out button in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'ds-dashboard',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  constructor(private authService: AuthService) {}
 
+  signOut() {
+    this.authService.signOut();
+  }
 }

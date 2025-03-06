5e22f660f19904d5487153f50adcd200f3b9ffeb
core2drew <calupesandro@gmail.com>
2025-03-06 19:48:26 +0800

    chore: add auth guard

---
+++ src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { map } from 'rxjs/operators';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.getUser().pipe(
+    map((user) => {
+      if (user) return true;
+      router.navigate(['/login']);
+      return false;
+    }),
+  );
+};

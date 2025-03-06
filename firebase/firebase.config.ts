34befb6fcd2d8dac81d71cd6688cdebb0a9a8655
core2drew <calupesandro@gmail.com>
2025-03-06 11:05:11 +0800

    chore: update firebase configs

--- firebase/firebase.config.ts
+++ firebase/firebase.config.ts
@@ -1,14 +1,14 @@
-import 'dotenv/config';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { environment } from '../environments/environment';
 
 export const firebaseConfig = {
-  apiKey: process.env['FIREBASE_API_KEY'],
-  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-  projectId: process.env['FIREBASE_PROJECT_ID'],
-  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-  appId: process.env['FIREBASE_APP_ID'],
+  apiKey: environment.apiKey,
+  authDomain: environment.appId,
+  projectId: environment.projectId,
+  storageBucket: environment.storageBucket,
+  messagingSenderId: environment.messagingSenderId,
+  appId: environment.appId,
 };
 
 const app = initializeApp(firebaseConfig);

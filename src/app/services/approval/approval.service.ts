5569146a4a9ff8f895f5b1715861cfc6cd6c3eb4
core2drew <calupesandro@gmail.com>
2025-03-07 10:58:27 +0800

    fix: circular dependencies

--- src/app/services/approval/approval.service.ts
+++ src/app/services/approval/approval.service.ts
@@ -1,22 +1,13 @@
 import { doc, getDoc } from 'firebase/firestore';
-import { MessageService } from 'primeng/api';
 
 import { Injectable } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
-import { Router } from '@angular/router';
-
-import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApprovalService {
-  constructor(
-    private messageService: MessageService,
-    private db: Firestore,
-    private router: Router,
-    private authService: AuthService,
-  ) {}
+  constructor(private db: Firestore) {}
 
   async isUserApproved(userId: string): Promise<boolean> {
     const userDoc = await getDoc(doc(this.db, 'users', userId));

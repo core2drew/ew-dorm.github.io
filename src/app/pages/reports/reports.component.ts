30d68c2e7186c7b9cbb5acf5a108cdf68ab1ed0d
core2drew <calupesandro@gmail.com>
2025-03-15 16:02:48 +0800

    chore: update report page

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+import { ReportService } from '../../services/report/report.service';
+import { ReportTableComponent } from './components/report-table/report-table.component';
+import { Report } from './models/report.model';
+
+@UntilDestroy()
 @Component({
   selector: 'ds-reports',
-  imports: [],
+  imports: [ReportTableComponent],
   templateUrl: './reports.component.html',
-  styleUrl: './reports.component.scss'
+  styleUrl: './reports.component.scss',
+  providers: [ReportService],
 })
-export class ReportsComponent {
+export class ReportsComponent implements OnInit {
+  dataSource: Report[] = [];
 
+  constructor(private reportService: ReportService) {}
+
+  ngOnInit() {
+    this.reportService.data$.pipe(untilDestroyed(this)).subscribe((data) => {
+      this.dataSource = data || []; // Update table with new real-time data
+    });
+    this.reportService.getRealTimeData();
+  }
 }

c71c83152abfdb9d57125e353a629f30f0e74e65
core2drew <calupesandro@gmail.com>
2025-03-16 17:29:00 +0800

    chore: add mock messages

--- src/app/shared/messages-drawer/messages-drawer.component.ts
+++ src/app/shared/messages-drawer/messages-drawer.component.ts
@@ -1,10 +1,11 @@
+import { CardModule } from 'primeng/card';
 import { DrawerModule } from 'primeng/drawer';
 
 import { Component, model } from '@angular/core';
 
 @Component({
   selector: 'ds-messages-drawer',
-  imports: [DrawerModule],
+  imports: [DrawerModule, CardModule],
   templateUrl: './messages-drawer.component.html',
   styleUrl: './messages-drawer.component.scss',
 })

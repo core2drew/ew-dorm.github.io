12720215bba9f8913b8ad0fe80addf273bb4330e
core2drew <calupesandro@gmail.com>
2025-03-16 16:43:12 +0800

    chore: update reports to handle filtering

--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,4 +1,9 @@
+import { DatePickerModule } from 'primeng/datepicker';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { filter } from 'rxjs';
+
 import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 import { ReportService } from '../../services/report/report.service';
@@ -8,13 +13,19 @@ import { Report } from './models/report.model';
 @UntilDestroy()
 @Component({
   selector: 'ds-reports',
-  imports: [ReportTableComponent],
+  imports: [
+    ReportTableComponent,
+    DatePickerModule,
+    ReactiveFormsModule,
+    FloatLabelModule,
+  ],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss',
   providers: [ReportService],
 })
 export class ReportsComponent implements OnInit {
   dataSource: Report[] = [];
+  dateRangeControl = new FormControl<Date[]>([]);
 
   constructor(private reportService: ReportService) {}
 
@@ -22,6 +33,23 @@ export class ReportsComponent implements OnInit {
     this.reportService.data$.pipe(untilDestroyed(this)).subscribe((data) => {
       this.dataSource = data || []; // Update table with new real-time data
     });
-    this.reportService.getRealTimeData();
+    this.reportService.init();
+
+    this.dateRangeControl.valueChanges
+      .pipe(
+        filter((arr) => {
+          if (
+            !Array.isArray(arr) ||
+            arr.length < 2 ||
+            arr.some((x) => x === null)
+          ) {
+            return false;
+          }
+          return true;
+        }),
+      )
+      .subscribe((dates) => {
+        this.reportService.filterByDate(dates as Date[]);
+      });
   }
 }

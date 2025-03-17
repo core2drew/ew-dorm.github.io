01ff8768387abdaa2e72964cc297923d867cf8b7
core2drew <calupesandro@gmail.com>
2025-03-17 17:53:02 +0800

    chore: add initial methods

---
+++ src/app/services/dashboard/dashboard.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DashboardService {
+  constructor() {}
+
+  getAllConsumptionData() {}
+
+  getTodayConsumption() {}
+
+  getWeeklyAverageConsumption() {}
+
+  getMonthlyAverageConsumption() {}
+
+  getAllMonthsConsumption() {}
+}

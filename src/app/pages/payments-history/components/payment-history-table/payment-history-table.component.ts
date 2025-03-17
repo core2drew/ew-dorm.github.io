1ac9c48a681bdc9021451bf19dc990ab775346d6
core2drew <calupesandro@gmail.com>
2025-03-17 14:33:06 +0800

    chore: create payment history table

---
+++ src/app/pages/payments-history/components/payment-history-table/payment-history-table.component.ts
@@ -0,0 +1,29 @@
+import { format } from 'date-fns';
+import { ChipModule } from 'primeng/chip';
+import { TableModule } from 'primeng/table';
+
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { PaymentHistory } from '../../models/payment-history.model';
+
+@Component({
+  selector: 'ds-payment-history-table',
+  imports: [TableModule, ChipModule, CommonModule],
+  templateUrl: './payment-history-table.component.html',
+  styleUrl: './payment-history-table.component.scss',
+})
+export class PaymentHistoryTableComponent implements OnInit {
+  @Input() data!: Array<PaymentHistory>;
+
+  ngOnInit(): void {
+    this.data = [
+      {
+        month: format(new Date(), 'MMMM'),
+        consumption: 20,
+        totalPrice: 500,
+        paymentStatus: true,
+      },
+    ];
+  }
+}
--- src/app/pages/payments-history/payments-history.component.ts
+++ src/app/pages/payments-history/payments-history.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
+import { PaymentHistoryTableComponent } from './components/payment-history-table/payment-history-table.component';
+
 @Component({
   selector: 'ds-payments-history',
-  imports: [],
+  imports: [PaymentHistoryTableComponent],
   templateUrl: './payments-history.component.html',
-  styleUrl: './payments-history.component.scss'
+  styleUrl: './payments-history.component.scss',
 })
-export class PaymentsHistoryComponent {
-
-}
+export class PaymentsHistoryComponent {}

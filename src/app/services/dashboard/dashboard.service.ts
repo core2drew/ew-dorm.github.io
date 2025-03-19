b8177f19ab0d2df80fa0ceae370259ca5bd35d36
core2drew <calupesandro@gmail.com>
2025-03-19 08:11:06 +0800

    chore: prepare readonly variables in dashboard service

--- src/app/services/dashboard/dashboard.service.ts
+++ src/app/services/dashboard/dashboard.service.ts
@@ -1,7 +1,31 @@
+import { BehaviorSubject } from 'rxjs';
+
 import { Injectable } from '@angular/core';
+import { select } from '@ngneat/elf';
+
+import { dashboardStore } from '../../pages/dashboard/store/dashboard.store';
+import { WaterConsumption } from '../../shared/models/water-consumption.model';
 
 @Injectable()
 export class DashboardService {
+  private dataSubject = new BehaviorSubject<Array<WaterConsumption> | null>(
+    null,
+  );
+  public data$ = this.dataSubject.asObservable();
+
+  readonly todayConsumption = dashboardStore.pipe(
+    select((state) => state.todayConsumption),
+  );
+  readonly weeklyAvgConsumption = dashboardStore.pipe(
+    select((state) => state.weeklyAvgConsumption),
+  );
+  readonly monthlyAvgConsumption = dashboardStore.pipe(
+    select((state) => state.monthlyAvgConsumption),
+  );
+  readonly allYearConsumption = dashboardStore.pipe(
+    select((state) => state.allYearConsumption),
+  );
+
   constructor() {}
 
   getAllConsumptionData() {

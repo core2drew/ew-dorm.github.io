6535f3f24ab966dcaad474b0791b8bb89d1292c4
core2drew <calupesandro@gmail.com>
2025-03-20 09:01:56 +0800

    chore: add state in dashboard store

--- src/app/pages/dashboard/store/dashboard.store.ts
+++ src/app/pages/dashboard/store/dashboard.store.ts
@@ -5,6 +5,10 @@ import { DashboardData } from './dashboard.model';
 export const dashboardStore = createStore(
   { name: 'dashboard' },
   withProps<DashboardData>({
+    loaded: false,
+    error: false,
+    saving: false,
+    loading: false,
     todayConsumption: 0,
     weeklyAvgConsumption: 0,
     monthlyAvgConsumption: 0,

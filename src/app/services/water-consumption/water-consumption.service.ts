7dc1b2c3d85c1de4454fe4d6838e5b7cbf6137ee
core2drew <calupesandro@gmail.com>
2025-03-20 21:36:36 +0800

    chore: create common service for water-consumption subscription

---
+++ src/app/services/water-consumption/water-consumption.service.ts
@@ -0,0 +1,52 @@
+import {
+  collection,
+  onSnapshot,
+  query,
+  QueryFieldFilterConstraint,
+  Timestamp,
+  Unsubscribe,
+} from 'firebase/firestore';
+import { BehaviorSubject } from 'rxjs';
+
+import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+
+import { WaterConsumption } from '../../shared/models/water-consumption.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WaterConsumptionService {
+  private dataSubject = new BehaviorSubject<Array<WaterConsumption> | null>(
+    null,
+  );
+  public data$ = this.dataSubject.asObservable();
+  public unsubscribe: Unsubscribe | undefined;
+
+  constructor(private db: Firestore) {}
+
+  subscribeToWaterConsumption(constraints: QueryFieldFilterConstraint[] = []) {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+
+    const q = query(collection(this.db, 'water_consumption'), ...constraints);
+
+    this.unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const formattedData: WaterConsumption[] = querySnapshot.docs.map(
+        (doc) => {
+          const rawData = doc.data() as WaterConsumption;
+          const timestamp = rawData.timestamp as unknown as Timestamp;
+          return {
+            id: doc.id,
+            uid: rawData.uid,
+            consumption: rawData.consumption,
+            timestamp: timestamp.toDate().toLocaleDateString(),
+          };
+        },
+      );
+      this.dataSubject.next(formattedData);
+      console.log('Real-time data:', formattedData);
+    });
+  }
+}

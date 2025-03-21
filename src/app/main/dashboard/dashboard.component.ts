da874b44f797e40e8fd2333a0b955254c8bde99c
core2drew <calupesandro@gmail.com>
2025-03-21 13:06:45 +0800

    chore: consume all months data

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -6,6 +6,7 @@ import { PushPipe } from '@ngrx/component';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { MetricCardComponent } from './components/metric-card/metric-card.component';
 import { DashboardService } from './services/dashboard.service';
+import { DashboardData } from './store/dashboard.model';
 
 @Component({
   selector: 'ds-dashboard',
@@ -21,37 +22,27 @@ export class DashboardComponent implements OnInit {
   todayConsumption$: Observable<number | undefined> | undefined;
   weeklyConsumption$: Observable<number | undefined> | undefined;
   monthlyConsumption$: Observable<number | undefined> | undefined;
-  allMonthsConsumption = this.dashboardService.allMonthsConsumption;
 
   ngOnInit() {
     this.todayConsumption$ = this.dashboardService.todayConsumption$;
     this.weeklyConsumption$ = this.dashboardService.weeklyConsumption$;
     this.monthlyConsumption$ = this.dashboardService.monthlyConsumption$;
 
-    console.log(this.allMonthsConsumption());
-    this.basicData = {
-      labels: [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-      ],
-      datasets: [
-        {
-          label: 'Water Consumption (m3)',
-          data: Array.from(
-            { length: 5 },
-            () => Math.floor(Math.random() * (200 - 100 + 1)) + 100,
-          ), // Random consumption between 100-200L
-          backgroundColor: 'rgba(75, 192, 192, 0.5)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
-        },
-      ],
-    };
+    this.dashboardService.allMonthsConsumption$.subscribe((monthData) => {
+      const { monthLabels: labels, data } =
+        monthData as DashboardData['allYearConsumption'];
+      this.basicData = {
+        labels,
+        datasets: [
+          {
+            label: 'Water Consumption (m3)',
+            data,
+            backgroundColor: 'rgba(75, 192, 192, 0.5)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+        ],
+      };
+    });
   }
 }
--- src/app/main/dashboard/services/dashboard.service.ts
+++ src/app/main/dashboard/services/dashboard.service.ts
@@ -1,12 +1,103 @@
-import { Injectable } from '@angular/core';
+import {
+  format,
+  isThisMonth,
+  isThisWeek,
+  isThisYear,
+  isToday,
+  toDate,
+} from 'date-fns';
+import { BehaviorSubject } from 'rxjs';
 
+import { Injectable, OnDestroy } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+
+import { WaterConsumption } from '../../../stores/water-consumption/water-consumption.model';
 import { WaterConsumptionRepository } from '../../../stores/water-consumption/water-consumption.repository';
+import { DashboardData } from '../store/dashboard.model';
 
+@UntilDestroy()
 @Injectable()
-export class DashboardService {
-  constructor(private waterConsumptionRepo: WaterConsumptionRepository) {}
+export class DashboardService implements OnDestroy {
+  todayConsumption$ = new BehaviorSubject<number | undefined>(undefined);
+  weeklyConsumption$ = new BehaviorSubject<number | undefined>(undefined);
+  monthlyConsumption$ = new BehaviorSubject<number | undefined>(undefined);
+  allMonthsConsumption$ = new BehaviorSubject<
+    DashboardData['allYearConsumption'] | null
+  >(null);
 
-  getTodayConsumption() {
-    this.waterConsumptionRepo.entities$.subscribe((d) => console.log(d));
+  private generateBarChartData(data: WaterConsumption[]) {
+    const groupedData = Object.groupBy(data, (item) => {
+      const [month, day, year] = item.timestamp.split('/');
+      return `${year}-${month}`;
+    }) as Record<string, WaterConsumption[]>;
+
+    const summedData = Object.keys(groupedData).reduce(
+      (acc, cur) => {
+        const date = toDate(new Date(cur));
+        const monthName = format(date, 'MMMM');
+        const year = format(date, 'Y');
+        const monthLabel = isThisYear(date)
+          ? monthName
+          : `${monthName} (${year})`;
+
+        const monthConsumption = groupedData[cur].reduce(
+          (_acc, _cur) => (_acc += _cur['consumption']),
+          0,
+        );
+        return {
+          monthLabels: [...acc['monthLabels'], monthLabel],
+          data: [...acc['data'], monthConsumption],
+        };
+      },
+      {
+        monthLabels: [] as string[],
+        data: [] as number[],
+      },
+    );
+
+    return summedData;
+  }
+
+  private getTodayConsumption(consumptions: WaterConsumption[]) {
+    return consumptions.reduce((acc, cur) => {
+      const { timestamp } = cur;
+      if (isToday(timestamp)) {
+        return (acc += cur.consumption);
+      }
+      return acc;
+    }, 0);
+  }
+
+  private getWeeklyConsumption(consumptions: WaterConsumption[]) {
+    return consumptions
+      .filter(({ timestamp }) => isThisWeek(timestamp, { weekStartsOn: 1 }))
+      .reduce((acc, cur) => {
+        return (acc += cur.consumption);
+      }, 0);
+  }
+
+  private getMonthlyConsumption(consumptions: WaterConsumption[]) {
+    return consumptions
+      .filter(({ timestamp }) => isThisMonth(timestamp))
+      .reduce((acc, cur) => {
+        return (acc += cur.consumption);
+      }, 0);
+  }
+
+  constructor(private waterConsumptionRepo: WaterConsumptionRepository) {
+    this.waterConsumptionRepo.entities$
+      .pipe(untilDestroyed(this))
+      .subscribe((consumptions) => {
+        this.todayConsumption$.next(this.getTodayConsumption(consumptions));
+        this.weeklyConsumption$.next(this.getWeeklyConsumption(consumptions));
+        this.monthlyConsumption$.next(this.getMonthlyConsumption(consumptions));
+        this.allMonthsConsumption$.next(
+          this.generateBarChartData(consumptions),
+        );
+      });
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroyed');
   }
 }

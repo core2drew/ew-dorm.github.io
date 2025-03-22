2fd335b8b51b2cc720bba9bff2f17f2046a88ace
core2drew <calupesandro@gmail.com>
2025-03-22 21:57:24 +0800

    chore: consume API

--- src/app/main/payments-history/payments-history.component.ts
+++ src/app/main/payments-history/payments-history.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { PaymentHistoryRepository } from '../../repositories/payment-history/payment-history.repository';
 import { PaymentHistoryTableComponent } from './components/payment-history-table/payment-history-table.component';
 
 @Component({
@@ -8,4 +9,10 @@ import { PaymentHistoryTableComponent } from './components/payment-history-table
   templateUrl: './payments-history.component.html',
   styleUrl: './payments-history.component.scss',
 })
-export class PaymentsHistoryComponent {}
+export class PaymentsHistoryComponent implements OnInit {
+  constructor(private paymentHistoryRepo: PaymentHistoryRepository) {}
+
+  ngOnInit(): void {
+    this.paymentHistoryRepo.getPaymentHistory();
+  }
+}

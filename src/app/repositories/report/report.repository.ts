c7aac538add8d265c24620770a6fe1239a0c2e43
core2drew <calupesandro@gmail.com>
2025-03-22 08:05:32 +0800

    chore: update report repository to get reports

--- src/app/repositories/report/report.repository.ts
+++ src/app/repositories/report/report.repository.ts
@@ -1,4 +1,11 @@
+import { combineLatest, map, Observable } from 'rxjs';
+
 import { Injectable } from '@angular/core';
+import { selectAllEntities } from '@ngneat/elf-entities';
+
+import { Report } from '../../shared/models/report.model';
+import { userStore } from '../../stores/user.store';
+import { waterConsumptionStore } from '../../stores/water-consumption.store';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +13,24 @@ import { Injectable } from '@angular/core';
 export class ReportRepository {
   constructor() {}
 
-  getReport() {}
+  getReport(): Observable<Report[]> {
+    const report$ = combineLatest({
+      waterConsumptions: waterConsumptionStore.pipe(selectAllEntities()),
+      users: userStore.pipe(selectAllEntities()),
+    }).pipe(
+      map(({ waterConsumptions, users }) => {
+        return waterConsumptions.map((waterConsumption) => {
+          const userConsumption = users.find(
+            (wc) => wc.id === waterConsumption.uid,
+          );
+          const { firstName, lastName } = userConsumption || {};
+          return {
+            ...waterConsumption,
+            tenantName: `${firstName} ${lastName}`,
+          } as Report;
+        });
+      }),
+    );
+    return report$;
+  }
 }

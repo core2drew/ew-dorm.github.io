8d21278e6d5ae5a21064b0ecabc38edad40cfdc9
core2drew <calupesandro@gmail.com>
2025-03-22 08:03:47 +0800

    chore: unused service

--- src/app/main/reports/reports.component.ts
+++ src/app/main/reports/reports.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { UntilDestroy } from '@ngneat/until-destroy';
 
-import { WaterConsumption } from '../../shared/models/water-consumption.model';
+import { ReportRepository } from '../../repositories/report/report.repository';
+import { Report } from '../../shared/models/report.model';
 import { ReportTableComponent } from './components/report-table/report-table.component';
 import { ReportService } from './report/report.service';
 
@@ -24,15 +25,14 @@ import { ReportService } from './report/report.service';
   providers: [ReportService],
 })
 export class ReportsComponent {
-  dataSource: WaterConsumption[] = [];
+  dataSource: Report[] = [];
   dateRangeControl = new FormControl<Date[]>([]);
 
-  constructor(private reportService: ReportService) {}
+  constructor(private reportRepo: ReportRepository) {}
 
   ngOnInit() {
-    this.reportService.data$.subscribe(
-      (data) => (this.dataSource = data as WaterConsumption[]),
-    );
+    this.reportRepo.getReport().subscribe((data) => (this.dataSource = data));
+
     this.dateRangeControl.valueChanges
       .pipe(
         filter((arr) => {

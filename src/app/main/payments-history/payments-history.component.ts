6fefd8325e773a02cc4bbd1e2495f439469bba09
core2drew <calupesandro@gmail.com>
2025-03-23 14:47:06 +0800

    chore: consume user repo data and display in select field

--- src/app/main/payments-history/payments-history.component.ts
+++ src/app/main/payments-history/payments-history.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
 import { PushPipe } from '@ngrx/component';
 
 import { PaymentHistoryRepository } from '../../repositories/payment-history/payment-history.repository';
+import { UserRepository } from '../../repositories/user/user.repository';
+import { User } from '../../shared/models/user.model';
 import { PaymentHistoryTableActionBarComponent } from './components/payment-history-table-action-bar/payment-history-table-action-bar.component';
 import { PaymentHistoryTableComponent } from './components/payment-history-table/payment-history-table.component';
 import { PaymentHistory } from './models/payment-history.model';
@@ -20,9 +22,15 @@ import { PaymentHistory } from './models/payment-history.model';
 })
 export class PaymentsHistoryComponent implements OnInit {
   dataSource$: Observable<PaymentHistory[]> = of([]);
-  constructor(private paymentHistoryRepo: PaymentHistoryRepository) {}
+  userDataSource$: Observable<User[]> = of([]);
+
+  constructor(
+    private paymentHistoryRepo: PaymentHistoryRepository,
+    private userRepo: UserRepository,
+  ) {}
 
   ngOnInit(): void {
+    this.userDataSource$ = this.userRepo.entities$;
     // this.paymentHistoryRepo.getPaymentHistory();
     this.dataSource$ = this.paymentHistoryRepo.data$;
   }

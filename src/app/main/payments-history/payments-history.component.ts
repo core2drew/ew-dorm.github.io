25ca9c1d8bd4e907ae64c99d97b4172edfd23025
core2drew <calupesandro@gmail.com>
2025-03-23 13:56:44 +0800

    chore: use action bar

--- src/app/main/payments-history/payments-history.component.ts
+++ src/app/main/payments-history/payments-history.component.ts
@@ -1,16 +1,20 @@
-import { ButtonModule } from 'primeng/button';
 import { Observable, of } from 'rxjs';
 
 import { Component, OnInit } from '@angular/core';
 import { PushPipe } from '@ngrx/component';
 
 import { PaymentHistoryRepository } from '../../repositories/payment-history/payment-history.repository';
+import { PaymentHistoryTableActionBarComponent } from './components/payment-history-table-action-bar/payment-history-table-action-bar.component';
 import { PaymentHistoryTableComponent } from './components/payment-history-table/payment-history-table.component';
 import { PaymentHistory } from './models/payment-history.model';
 
 @Component({
   selector: 'ds-payments-history',
-  imports: [PaymentHistoryTableComponent, PushPipe, ButtonModule],
+  imports: [
+    PaymentHistoryTableComponent,
+    PushPipe,
+    PaymentHistoryTableActionBarComponent,
+  ],
   templateUrl: './payments-history.component.html',
   styleUrl: './payments-history.component.scss',
 })
@@ -19,7 +23,7 @@ export class PaymentsHistoryComponent implements OnInit {
   constructor(private paymentHistoryRepo: PaymentHistoryRepository) {}
 
   ngOnInit(): void {
-    this.paymentHistoryRepo.getPaymentHistory();
+    // this.paymentHistoryRepo.getPaymentHistory();
     this.dataSource$ = this.paymentHistoryRepo.data$;
   }
 }

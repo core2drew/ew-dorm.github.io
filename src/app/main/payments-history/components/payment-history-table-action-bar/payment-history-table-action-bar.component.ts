9aaf37a185d5a44a9ad01a241ef1f3a64c172cca
core2drew <calupesandro@gmail.com>
2025-03-24 07:40:33 +0800

    chore: remove create button and disallow clear select field

--- src/app/main/payments-history/components/payment-history-table-action-bar/payment-history-table-action-bar.component.ts
+++ src/app/main/payments-history/components/payment-history-table-action-bar/payment-history-table-action-bar.component.ts
@@ -1,24 +1,30 @@
 import { ButtonModule } from 'primeng/button';
+import { IftaLabelModule } from 'primeng/iftalabel';
 import { SelectModule } from 'primeng/select';
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import { User } from '../../../../shared/models/user.model';
 
 @Component({
   selector: 'ds-payment-history-table-action-bar',
-  imports: [SelectModule, ButtonModule, ReactiveFormsModule],
+  imports: [SelectModule, ButtonModule, ReactiveFormsModule, IftaLabelModule],
   templateUrl: './payment-history-table-action-bar.component.html',
   styleUrl: './payment-history-table-action-bar.component.scss',
 })
 export class PaymentHistoryTableActionBarComponent implements OnInit {
   @Input() users: User[] | undefined;
   @Input() activeUser: User | undefined;
+  @Output() changeTenantEvent = new EventEmitter<string | null>();
 
   nameControl = new FormControl<string | undefined>(undefined);
 
   ngOnInit(): void {
     this.nameControl.setValue(this.activeUser?.name);
+
+    this.nameControl.valueChanges.subscribe((value) => {
+      this.changeTenantEvent.emit(value);
+    });
   }
 }

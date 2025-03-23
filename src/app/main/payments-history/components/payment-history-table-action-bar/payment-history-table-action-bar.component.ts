9fe0866ec4b7395c3802e972407430fa76fc2b59
core2drew <calupesandro@gmail.com>
2025-03-23 17:27:35 +0800

    chore: add active user input and set it to dropdown

--- src/app/main/payments-history/components/payment-history-table-action-bar/payment-history-table-action-bar.component.ts
+++ src/app/main/payments-history/components/payment-history-table-action-bar/payment-history-table-action-bar.component.ts
@@ -1,16 +1,24 @@
 import { ButtonModule } from 'primeng/button';
 import { SelectModule } from 'primeng/select';
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import { User } from '../../../../shared/models/user.model';
 
 @Component({
   selector: 'ds-payment-history-table-action-bar',
-  imports: [SelectModule, ButtonModule],
+  imports: [SelectModule, ButtonModule, ReactiveFormsModule],
   templateUrl: './payment-history-table-action-bar.component.html',
   styleUrl: './payment-history-table-action-bar.component.scss',
 })
-export class PaymentHistoryTableActionBarComponent {
+export class PaymentHistoryTableActionBarComponent implements OnInit {
   @Input() users: User[] | undefined;
+  @Input() activeUser: User | undefined;
+
+  nameControl = new FormControl<string | undefined>(undefined);
+
+  ngOnInit(): void {
+    this.nameControl.setValue(this.activeUser?.name);
+  }
 }

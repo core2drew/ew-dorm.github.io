2dc85bb170630dc259eee1de9ca47d6e440a019c
core2drew <calupesandro@gmail.com>
2025-03-24 19:39:57 +0800

    fix: missing process

--- src/app/app.token.ts
+++ src/app/app.token.ts
@@ -2,7 +2,7 @@ import { InjectionToken } from '@angular/core';
 
 import { environment } from '../environments/environment';
 
-const apiUrl = process.env['api_url'] || environment.api.url;
+const apiUrl = environment.api.url;
 
 export const API_URL = new InjectionToken<string>('api-url', {
   factory: () => apiUrl,

896953ba477a375b21cade408617e6dfa7c36535
core2drew <calupesandro@gmail.com>
2025-03-24 08:06:39 +0800

    chore: create pay now button

--- src/app/main/payments-history/components/payment-history-table/payment-history-table.component.ts
+++ src/app/main/payments-history/components/payment-history-table/payment-history-table.component.ts
@@ -1,4 +1,7 @@
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 import { ChipModule } from 'primeng/chip';
+import { MenuModule } from 'primeng/menu';
 import { TableModule } from 'primeng/table';
 
 import { CommonModule } from '@angular/common';
@@ -8,12 +11,29 @@ import { PaymentHistory } from '../../models/payment-history.model';
 
 @Component({
   selector: 'ds-payment-history-table',
-  imports: [TableModule, ChipModule, CommonModule],
+  imports: [TableModule, ChipModule, CommonModule, ButtonModule, MenuModule],
   templateUrl: './payment-history-table.component.html',
   styleUrl: './payment-history-table.component.scss',
 })
 export class PaymentHistoryTableComponent implements OnInit {
   @Input() data!: PaymentHistory[] | undefined;
+  items: MenuItem[] | undefined;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Options',
+        items: [
+          {
+            label: 'Refresh',
+            icon: 'pi pi-refresh',
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-upload',
+          },
+        ],
+      },
+    ];
+  }
 }

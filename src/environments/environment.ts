6a2df9127a5714bd93373590bfa15e1f4b088190
core2drew <calupesandro@gmail.com>
2025-03-24 12:59:50 +0800

    chore: push environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyABFD3y8YNfFth0sx54S036AITTNWuZSW4',
+    authDomain: 'dorm-system-bf714.firebaseapp.com',
+    projectId: 'dorm-system-bf714',
+    storageBucket: 'dorm-system-bf714.firebasestorage.app',
+    messagingSenderId: '528294967959',
+    appId: '1:528294967959:web:bc5bee056a309177e3c7d2',
+  },
+  api: {
+    url: 'http://localhost:4200/api',
+  },
+};

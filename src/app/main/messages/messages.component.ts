253cbc110cbdfa8a1ef54256de0f2ef884dc59f3
core2drew <calupesandro@gmail.com>
2025-03-30 16:33:50 +0800

    chore: integrate create message dialog - update message model

--- src/app/main/messages/messages.component.ts
+++ src/app/main/messages/messages.component.ts
@@ -1,50 +1,53 @@
 import { ButtonModule } from 'primeng/button';
 
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { CreateMessageDialogComponent } from './components/create-message-dialog/create-message-dialog.component';
 import { MessageHistoryTableComponent } from './components/message-history-table/message-history-table.component';
 import { Message } from './models/message.model';
 
 @Component({
   selector: 'ds-messages',
-  imports: [MessageHistoryTableComponent, ButtonModule],
+  imports: [
+    MessageHistoryTableComponent,
+    ButtonModule,
+    CreateMessageDialogComponent,
+    FormsModule,
+  ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss',
 })
 export class MessagesComponent {
+  isCreateMessageDialogVisible = false;
   data: Message[] = [
     {
-      subject: 'Team Meeting Reminder',
       message:
         "Don't forget our team meeting at 2 PM today in the conference room.",
       timestamp: '2023-10-27T13:00:00Z',
-      recipients: ['Hinata'],
+      recipientsName: ['Hinata'],
     },
     {
-      subject: 'Project Update',
       message:
         "Here's the latest update on the project. Please review the attached document.",
       timestamp: '2023-10-26T10:30:00Z',
-      recipients: ['Leo Oreo', 'Gong'],
+      recipientsName: ['Leo Oreo', 'Gong'],
     },
     {
-      subject: 'Welcome to the Team!',
       message: "Welcome aboard! We're excited to have you join our team.",
       timestamp: '2023-10-25T16:45:00Z',
-      recipients: ['Gong'],
+      recipientsName: ['Gong'],
     },
     {
-      subject: 'System Maintenance',
       message:
         'Please be advised that system maintenance will occur tonight from 12 AM to 2 AM. Services will be unavailable during this time.',
       timestamp: '2023-10-28T09:00:00Z',
-      recipients: ['Keluwang'],
+      recipientsName: ['Keluwang'],
     },
     {
-      subject: 'Holiday Schedule',
       message: 'Please note that the office will be closed on [Holiday Date].',
       timestamp: '2023-10-24T11:00:00Z',
-      recipients: ['Kurapekal'],
+      recipientsName: ['Kurapekal'],
     },
   ];
 }

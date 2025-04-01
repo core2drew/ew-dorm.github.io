09863c4f3de446f8350bd0bf0bb72eb67c70d842
core2drew <calupesandro@gmail.com>
2025-04-01 10:01:58 +0800

    chore: integrate user table and dialog in users page

--- src/app/main/users/users.component.ts
+++ src/app/main/users/users.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+import { Component, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+import { UserRepository } from '../../repositories/user/user.repository';
+import { User } from '../../shared/models/user.model';
+import { UsersDialogComponent } from './components/users-dialog/users-dialog.component';
+import { UsersTableComponent } from './components/users-table/users-table.component';
 
 @Component({
   selector: 'ds-users',
-  imports: [],
+  imports: [UsersTableComponent, ButtonModule, UsersDialogComponent],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrl: './users.component.scss',
 })
 export class UsersComponent {
+  isUserDialogVisible = false;
+  isUpdateMode = false;
+  $users: Signal<User[] | undefined> | undefined;
 
+  constructor(private userRepo: UserRepository) {
+    this.$users = toSignal(this.userRepo.entities$);
+  }
+
+  updateUser() {
+    this.isUserDialogVisible = true;
+    this.isUpdateMode = false;
+  }
 }

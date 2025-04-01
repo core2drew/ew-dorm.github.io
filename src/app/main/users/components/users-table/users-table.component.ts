a2ccfa8d0baa86bfac57bf7aa796212e18ca37c8
core2drew <calupesandro@gmail.com>
2025-04-01 10:01:18 +0800

    chore: create user table

---
+++ src/app/main/users/components/users-table/users-table.component.ts
@@ -0,0 +1,23 @@
+import { Popover, PopoverModule } from 'primeng/popover';
+import { TableModule } from 'primeng/table';
+
+import { Component, Input, ViewChild } from '@angular/core';
+
+import { User } from '../../../../shared/models/user.model';
+
+@Component({
+  selector: 'ds-users-table',
+  imports: [TableModule, PopoverModule],
+  templateUrl: './users-table.component.html',
+  styleUrl: './users-table.component.scss',
+})
+export class UsersTableComponent {
+  @Input() users: User[] | undefined;
+
+  @ViewChild('op') op!: Popover;
+
+  toggle(event: Event) {
+    console.log(event);
+    this.op.toggle(event);
+  }
+}

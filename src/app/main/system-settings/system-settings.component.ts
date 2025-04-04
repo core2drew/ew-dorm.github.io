0f52f604c7cd10702c848bf13cc1c005e462a616
core2drew <calupesandro@gmail.com>
2025-04-04 19:24:49 +0800

    chore: create sys settings

---
+++ src/app/main/system-settings/system-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-system-settings',
+  imports: [],
+  templateUrl: './system-settings.component.html',
+  styleUrl: './system-settings.component.scss'
+})
+export class SystemSettingsComponent {
+
+}

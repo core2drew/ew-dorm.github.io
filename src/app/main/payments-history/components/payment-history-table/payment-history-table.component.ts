4c9612d6e54265748ddaf4ba58027592b9fa11fd
core2drew <calupesandro@gmail.com>
2025-04-05 13:24:19 +0800

    chore: format currency

--- src/app/main/payments-history/components/payment-history-table/payment-history-table.component.ts
+++ src/app/main/payments-history/components/payment-history-table/payment-history-table.component.ts
@@ -3,14 +3,22 @@ import { ChipModule } from 'primeng/chip';
 import { MenuModule } from 'primeng/menu';
 import { TableModule } from 'primeng/table';
 
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe, DecimalPipe } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { PaymentHistory } from '../../models/payment-history.model';
 
 @Component({
   selector: 'ds-payment-history-table',
-  imports: [TableModule, ChipModule, CommonModule, ButtonModule, MenuModule],
+  imports: [
+    TableModule,
+    ChipModule,
+    CommonModule,
+    ButtonModule,
+    MenuModule,
+    CurrencyPipe,
+    DecimalPipe,
+  ],
   templateUrl: './payment-history-table.component.html',
   styleUrl: './payment-history-table.component.scss',
 })
